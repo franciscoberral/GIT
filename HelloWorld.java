985c540bb03e5e2a6e152f3a6a73263522e47be2
Francisco <fberzaf1908@gmail.com>
2021-10-27 14:22:33 +0200

    Ejemplo

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+    public static void main(String args[]){
+        System.out.println("Hola Mundo"); //print
+        //equivalente a lo que hemos visto de print
+        //javac HelloWorld.java
+        //java HelloWorld
+    }
+}
