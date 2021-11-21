08975ccbccde90478b2545e77da6461ab66ebacb
franciscoberral <fberzaf1908@g.educaand.es>
2021-11-21 17:07:57 +0100

    Ejercicios

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld{
-    public static void main(String args[]){
-        System.out.println("Hola Mundo"); //print
-        //equivalente a lo que hemos visto de print
-        //javac HelloWorld.java
-        //java HelloWorld
-    }
-}
